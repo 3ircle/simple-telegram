0c4f0e3506522b62bc267d1c87c22738c4f00fd9
Ali momenzadeh <c3ircle@outlook.com>
2025-11-25 00:33:58 +0330

    Update bot API key to placeholder
    Replaced the bot API key with a placeholder.

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 import simple_telegram
 
-bot = simple_telegram.bot.Bot('8272608766:AAHNWSeQ88o6ALppQiY_4U_DwPe9zw28rEo')
+bot = simple_telegram.bot.Bot('YOUR_API_KEY')
 
 def message_handler(update):
     if update.message and update.message.text == 'hi':
